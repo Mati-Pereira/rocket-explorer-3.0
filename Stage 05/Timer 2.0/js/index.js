fe1ecf4adde744227374052f3945d0d1720610eb
Matheus <94717377+Mati-Pereira@users.noreply.github.com>
2022-07-16 21:02:18 -0300

    finishing

--- Stage 05/Timer 2.0/js/index.js
+++ Stage 05/Timer 2.0/js/index.js
@@ -1,29 +1,3 @@
-// sounds
-
-const floresta = document.querySelector(".floresta")
-const chuva = document.querySelector(".chuva")
-const cafeteria = document.querySelector(".cafeteria")
-const lareira = document.querySelector(".lareira")
-
-const florestaSound = document.querySelector(".florestaSound")
-const chuvaSound = document.querySelector(".chuvaSound")
-const cafeteriaSound = document.querySelector(".cafeteriaSound")
-const lareiraSound = document.querySelector(".lareiraSound")
-
-const florestaHover = document.querySelector(".florestaHover")
-const chuvaHover = document.querySelector(".chuvaHover")
-const cafeteriaHover = document.querySelector(".cafeteriaHover")
-const lareiraHover = document.querySelector(".lareiraHover")
-
-const minutesDisplay = document.querySelector(".minutesDisplay")
-const secondsDisplay = document.querySelector(".secondsDisplay")
-
-const playButton = document.querySelector(".playButton")
-const stopButton = document.querySelector(".stopButton")
-const plusButton = document.querySelector(".plusButton")
-const minusButton = document.querySelector(".minusButton")
-
-
 floresta.addEventListener('mouseover', () => {
   florestaSound.classList.add("hidden")
   florestaHover.classList.remove("hidden")
@@ -102,11 +76,6 @@ lareira.addEventListener('click', () => {
   resetAudio()
   lareiraAudio.play()
 })
-
-
-let seconds = secondsDisplay
-let minutes = minutesDisplay
-let counter
 
 
 playButton.addEventListener("click", () => {
